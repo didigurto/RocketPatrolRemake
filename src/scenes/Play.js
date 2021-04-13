4c45f845821bce182b56ee5c1bdceb113eb58d1c
Your Name <student@ucsc.edu>
2021-04-13 13:13:33 -0700

    Added starfield

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,8 +4,18 @@ class Play extends Phaser.Scene{
         console.log("is this working");
     }
 
+    preload() {
+        this.load.image('starfield', 'assets/starfield.png');
+        console.log("dkdkdk")
+    }
+
         
 create(){
+    //adding starfield and specifying how much of the image to use as a tile sprite
+    this.starfield = this.add.tileSprite(
+        0,0,640,480, 'starfield').setOrigin(0,0);
+    
+
     // green UI background
    this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00,).setOrigin(0,0);
     //white borders
@@ -13,6 +23,7 @@ create(){
 	this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
 	this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+
 
 }
 
