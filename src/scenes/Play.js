68d2c832d73af036450be2b632510e5132302d48
Your Name <student@ucsc.edu>
2021-04-13 17:31:10 -0700

    Added controllable p1 rocket

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,7 +6,7 @@ class Play extends Phaser.Scene{
 
     preload() {
         this.load.image('starfield', 'assets/starfield.png');
-        console.log("dkdkdk")
+        this.load.image('rocket', 'assets/rocket.png');
     }
 
         
@@ -15,6 +15,8 @@ create(){
     this.starfield = this.add.tileSprite(
         0,0,640,480, 'starfield').setOrigin(0,0);
     
+    this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket');
+
 
     // green UI background
    this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00,).setOrigin(0,0);
@@ -24,10 +26,16 @@ create(){
 	this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 	this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
 
+    keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+    keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+    keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+
 }
 //making starfield scroll by moving and phaser provides a function for it to repeat
 update() {
     this.starfield.tilePositionX += 4;
+    this.p1Rocket.update();
 }
 
 
